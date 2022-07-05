9a5f0c305da3b8b8b155395ed8f2a411743ade37
rbbeny <benjaramirez2501@gmail.com>
2022-07-05 12:25:06 -0500

    cambio donde chocaba

--- resources/views/GerenteCobranza/TablaUsuariosGC.blade.php
+++ resources/views/GerenteCobranza/TablaUsuariosGC.blade.php
@@ -34,31 +34,6 @@
         <div class="modal-header">
           <h5 class="modal-title" id="staticBackdropLabel">agregar usuario</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-<nav aria-label="breadcrumb" style="padding: 10px;">
-  <ol class="breadcrumb">
-    <li class="breadcrumb-item"><a href="/homeCobranza">Home</a></li>
-    <li class="breadcrumb-item active" aria-current="page">Ver Usuarios</li>
-  </ol>
-</nav>    
-<!-- Button trigger modal -->
-<div align="right" style="padding-right: 40px;">
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-  Agregar usuario
-</button>
-</div>
-<!-- Modal ag_regar usuario -->
-<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="staticBackdropLabel">Agregar usuario</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-      <form>
-        <div class="mb-3">
-          <label   class="form-label">Nombre</label>
-          <input   type="text" id="ag_1" class="form-control" >
         </div>
         <div class="modal-body">
           <form action="/register" method="POST">
@@ -96,53 +71,12 @@
             <button id="btn" type="submit" class="btn btn-primary btn-block">Submit</button>
           </form>
         </div>
-        <div class="mb-3">
-          <label  class="form-label">Apellido Materno</label>
-          <input  id="ag_3" type="text" class="form-control">
-        </div>
-        <div class="mb-3">
-          <label  class="form-label" >Nickname</label>
-          <input  id="ag_4" placeholder="Kevyn69" type="text" class="form-control">
-        </div>
-        <div class="mb-3">
-          <label  class="form-label">Rol</label>
-                <select id="ag_5" class="form-select" aria-label=" select example" disabled>
-                      <option selected value="">Cobrador</option>
-                    </select>
-        </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">Password</label>
-          <input type="password" id="ag_6" class="form-control" id="exampleInputPassword1">
-        </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">Confirmar Password</label>
-          <input type="password" id="ag_7" class="form-control" id="exampleInputPassword1">
-        </div>
-        <button id="btn" type="submit" class="btn btn-primary btn-block" disabled>Submit</button>
-      </form>
       </div>
     </div>
   </div>
 
   <div class="tablaclientes">
 
-    <table id="clienteVentas" class="table display table-striped table-bordered nowrap" style="width:100%">
-      <thead>
-        <tr>
-          <th scope="col">Nombre</th>
-          <th scope="col">Rol</th>
-          <th scope="col">Nickname</th>
-          <th scope="col">Opciones</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Adrian Eduardo Villanueva</td>
-          <td>Administrador</td>
-          <td>adrian.villanueva</td>
-          <td><a data-bs-toggle="modal" onclick="eliminar()"><i style="font-size:25px; color:red;" class="bi bi-trash"></i></a>
-            <a data-bs-toggle="modal" data-bs-target="#EditarUsuario"><i style="font-size:25px; color:blue;" class="bi bi-pencil-square"></i></a>
-          </td>
         <table id="clienteVentas" class="table display table-striped table-bordered nowrap" style="width:100%">
             <thead>
                 <tr>
@@ -178,38 +112,6 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <form>
-          <div class="mb-3">
-            <label class="form-label">Nombre</label>
-            <input type="text" value="Adrian Eduardo" class="form-control">
-          </div>
-          <div class="mb-3">
-            <label class="form-label">Apellido Paterno</label>
-            <input type="text" value="Villanueva" class="form-control">
-          </div>
-          <div class="mb-3">
-            <label class="form-label">Apellido Materno</label>
-            <input type="text" value="Hernandez" class="form-control">
-          </div>
-          <div class="mb-3">
-            <label class="form-label">Nickname</label>
-            <input placeholder="Kevyn69" value="adrian.villanueva" type="text" class="form-control">
-          </div>
-          <div class="mb-3">
-            <label class="form-label">Rol</label>
-            <select class="form-select" aria-label=" select example">
-              <option selected value="Administrador"></option>
-              <option value="1">Administrador</option>
-              <option value="2">Gerente de ventas</option>
-              <option value="3">Gerente de cobranza</option>
-              <option value="4">Ventas</option>
-              <option value="5">Cobranza</option>
-              <option value="6">RH</option>
-              <option value="7">Finanzas</option>
-            </select>
-          </div>
-          <button type="submit" class="btn btn-primary btn-block">Submit</button>
-        </form>
       <form>
         <div class="mb-3">
           <label  class="form-label" >Nombre</label>
