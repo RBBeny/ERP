33efefef739ff8db9eb26874a42b3739ac263279
KevynC0rt <kevync0rtezr@gmail.com>
2022-07-28 22:51:55 -0500

    Se agrego la vista de reportes de gerente ventas

--- resources/views/GerenteVentas/GenerarReporte.blade.php
+++ resources/views/GerenteVentas/GenerarReporte.blade.php
@@ -23,70 +23,237 @@
     </nav>
     <div class="cabezera_consulta">
         <div class="contIzq">
-            @foreach($Fecha as $Fecha)
-        <h1 class="Fecha">{{$Fecha}}</h1>
-        @endforeach
+
+            <h1 class="Fecha">{{$Fecha}}</h1>
+
         </div>
         <div class="contDer">
-        <button type="button" class="btn btn-primary">Consultar</button>
+            <button type="button" class="btn btn-primary" id="abrir-popupReporte">
+                Consultar ventas
+            </button>
+
         </div>
     </div>
     <div class="cards">
-    <div class="card">
-        <div class="left-column background1-left-column">
-        <i class="fa-solid fa-user-tie fa-5x"></i>
-        </div>
+        <div class="cards_fila">
+            <div class="card">
+                <div class="left-column background1-left-column">
+                    <i class="fa-solid fa-user-tie fa-5x"></i>
+                </div>
 
-        <div class="right-column">
-            
-           <button></button>
+                <div class="right-column">
+                    <h3>Nombre del vendedor</h3>
+                    <p>Cuentas vendidas</p>
+                    <button class="button background1-left-column">Ver cuentas</button>
+                </div>
+
+            </div>
+
+            <div class="card">
+                <div class="left-column background2-left-column">
+                    <i class="fa-solid fa-user-tie fa-5x"></i>
+                </div>
+
+                <div class="right-column">
+                    <h3>Nombre del vendedor</h3>
+                    <p>Cuentas vendidas</p>
+                    <button class="button background2-left-column">Ver Cuentas</button>
+                </div>
+
+            </div>
+            <div class="card">
+                <div class="left-column background1-left-column">
+                    <i class="fa-solid fa-user-tie fa-5x"></i>
+                </div>
+
+                <div class="right-column">
+                    <h3>Nombre del vendedor</h3>
+                    <p>Cuentas vendidas</p>
+                    <button class="button background1-left-column">Ver cuentas</button>
+                </div>
+
+            </div>
+        </div>
+        <div class="cards_fila">
+            <div class="card">
+                <div class="left-column background1-left-column">
+                    <i class="fa-solid fa-user-tie fa-5x"></i>
+                </div>
+
+                <div class="right-column">
+                    <h3>Nombre del vendedor</h3>
+                    <p>Cuentas vendidas</p>
+                    <button class="button background1-left-column">Ver cuentas</button>
+                </div>
+
+            </div>
+
+            <div class="card">
+                <div class="left-column background2-left-column">
+                    <i class="fa-solid fa-user-tie fa-5x"></i>
+                </div>
+
+                <div class="right-column">
+                    <h3>Nombre del vendedor</h3>
+                    <p>Cuentas vendidas</p>
+                    <button class="button background2-left-column">Ver Cuentas</button>
+                </div>
+
+            </div>
+            <div class="card">
+                <div class="left-column background1-left-column">
+                    <i class="fa-solid fa-user-tie fa-5x"></i>
+                </div>
+
+                <div class="right-column">
+                    <h3>Nombre del vendedor</h3>
+                    <p>Cuentas vendidas</p>
+                    <button class="button background1-left-column">Ver cuentas</button>
+                </div>
+
+            </div>
+        </div>
+        <div class="cards_fila">
+            <div class="card">
+                <div class="left-column background1-left-column">
+                    <i class="fa-solid fa-user-tie fa-5x"></i>
+                </div>
+
+                <div class="right-column">
+                    <h3>Nombre del vendedor</h3>
+                    <p>Cuentas vendidas</p>
+                    <button class="button background1-left-column">Ver cuentas</button>
+                </div>
+
+            </div>
+
+            <div class="card">
+                <div class="left-column background2-left-column">
+                    <i class="fa-solid fa-user-tie fa-5x"></i>
+                </div>
+
+                <div class="right-column">
+                    <h3>Nombre del vendedor</h3>
+                    <p>Cuentas vendidas</p>
+                    <button class="button background2-left-column">Ver Cuentas</button>
+                </div>
+
+            </div>
+            <div class="card">
+                <div class="left-column background1-left-column">
+                    <i class="fa-solid fa-user-tie fa-5x"></i>
+                </div>
+
+                <div class="right-column">
+                    <h3>Nombre del vendedor</h3>
+                    <p>Cuentas vendidas</p>
+                    <button class="button background1-left-column">Ver cuentas</button>
+                </div>
+
+            </div>
+        </div>
+        <div class="cards_fila">
+            <div class="card">
+                <div class="left-column background1-left-column">
+                    <i class="fa-solid fa-user-tie fa-5x"></i>
+                </div>
+
+                <div class="right-column">
+                    <h3>Nombre del vendedor</h3>
+                    <p>Cuentas vendidas</p>
+                    <button class="button background1-left-column">Ver cuentas</button>
+                </div>
+
+            </div>
+
+            <div class="card">
+                <div class="left-column background2-left-column">
+                    <i class="fa-solid fa-user-tie fa-5x"></i>
+                </div>
+
+                <div class="right-column">
+                    <h3>Nombre del vendedor</h3>
+                    <p>Cuentas vendidas</p>
+                    <button class="button background2-left-column">Ver Cuentas</button>
+                </div>
+
+            </div>
+            <div class="card">
+                <div class="left-column background1-left-column">
+                    <i class="fa-solid fa-user-tie fa-5x"></i>
+                </div>
+
+                <div class="right-column">
+                    <h3>Nombre del vendedor</h3>
+                    <p>Cuentas vendidas</p>
+                    <button class="button background1-left-column">Ver cuentas</button>
+                </div>
+
+            </div>
+        </div>
+        <div class="cards_fila">
+            <div class="card">
+                <div class="left-column background1-left-column">
+                    <i class="fa-solid fa-user-tie fa-5x"></i>
+                </div>
+
+                <div class="right-column">
+                    <h3>Nombre del vendedor</h3>
+                    <p>Cuentas vendidas</p>
+                    <button class="button background1-left-column">Ver cuentas</button>
+                </div>
+
+            </div>
+
+            <div class="card">
+                <div class="left-column background2-left-column">
+                    <i class="fa-solid fa-user-tie fa-5x"></i>
+                </div>
+
+                <div class="right-column">
+                    <h3>Nombre del vendedor</h3>
+                    <p>Cuentas vendidas</p>
+                    <button class="button background2-left-column">Ver Cuentas</button>
+                </div>
+
+            </div>
+            <div class="card">
+                <div class="left-column background1-left-column">
+                    <i class="fa-solid fa-user-tie fa-5x"></i>
+                </div>
+
+                <div class="right-column">
+                    <h3>Nombre del vendedor</h3>
+                    <p>Cuentas vendidas</p>
+                    <button class="button background1-left-column">Ver cuentas</button>
+                </div>
+
+            </div>
         </div>
 
     </div>
 
-    <div class="card">
-        <div class="left-column background2-left-column">
-        <i class="fa-solid fa-user-tie fa-5x"></i>
-        </div>
 
-        <div class="right-column">
-            
-            <button class="button background2-left-column">Empezar</button>
-        </div>
 
+</div>
+<div class="overlay" id="overlayReporte">
+    <div class="popup" id="popupReporte">
+        <h2>Generar reporte</h2>
+        <div class="form-group">
+            <button type="button" id="btn-cerrar-popupReporte" class="btn btn-danger">Cancelar</button>
+            <button type="button" id="guardarReporte" class="btn btn-success">Guardar</button>
+        </div>
     </div>
-    <div class="card">
-        <div class="left-column background1-left-column">
-        <i class="fa-solid fa-user-tie fa-5x"></i>
-        </div>
-
-        <div class="right-column">
-            
-           <button></button>
-        </div>
-
-    </div>
-   
-   
-    </div>
-
+</div>
 
 </div>
 
 @section('js')
-<script src="js/GerenteCobranza/agregarUsuario.js"></script>
-app/Http/Controllers/VentasGerenteController.php<script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js" type="text/javascript"></script>
+<script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js" type="text/javascript"></script>
 <script src="https://cdn.datatables.net/1.11.4/js/dataTables.bootstrap5.min.js" type="text/javascript"></script>
+<script src="js/VentasGerente/estilos.js"></script>
 <script type="text/javascript">
-    $(document).ready(function() {
-        $('#clienteVentas').DataTable({
-            language: {
-                "url": "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
-            }
-        });
 
-
-    });
 </script>
 
 
