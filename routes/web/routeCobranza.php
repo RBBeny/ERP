cf61902f9fef3af7b7f1674569b1727ef30950b8
rbbeny <benjaramirez2501@gmail.com>
2022-08-02 16:56:12 -0500

    routes corrigidas

--- routes/web/routeCobranza.php
+++ routes/web/routeCobranza.php
@@ -50,5 +50,17 @@ Route::group(['middleware' => 'isCobranza'], function(){
     //Route:: get('/TablaClientesGC', [CobranzaGerenteController::class, 'clientes']);
     //Route:: get('/GenerarReporte', [CobranzaGerenteController::class, 'Reporte']);
     //Route:: get('/VerClienteGC/{id}', [CobranzaGerenteController::class, 'cliente']);
+
+    Route:: get('/homeCobranza', [CobranzaController::class, 'home']);
+    Route:: get('/PCobranza', [CobranzaController::class, 'PCobranza']);
+    Route:: get('/ClientesCobranza',[CobranzaController::class, 'clientes']);
+    Route:: get('/PagosCobranza',[CobranzaController::class, 'PagosCobranza']);
+    Route:: get('/Recibos',[CobranzaController::class, 'Recibos']);
+    Route:: get('/ClienteCobranza',[CobranzaController::class, 'ClienteCobranza']);
+    Route:: get('/TablasClientesC',[CobranzaController::class, 'TablasClientesC']);
+    Route:: get('/VerCliente/{id}', [CobranzaController::class, 'cliente']);
+    Route::get('delete/{cvePago}',[CobranzaController::class,'eliminarPago'])->name('eliminarPago');
+    Route::post('/Ppago', [CobranzaController::class, 'registerC']);
+    Route::post('/PagosC', [CobranzaController::class, 'registerPC']);
 });
 
