b5bfe6112385fafb87173433358a133dcfeadb27
KevynC0rt <kevync0rtezr@gmail.com>
2022-08-07 17:09:31 -0500

    Se modificaron los modelos de Estatus y Usuario

--- app/Models/Estatus.php
+++ app/Models/Estatus.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Cobrador extends Model
+class Estatus extends Model
 {
     //cveEstatus, nomEstatus
     protected $table = "cEstatus";
